00785916c49e09ccfdfeb97f838bad597b38d2ec
N0str4 <aymerickvalois@yahoo.fr>
2020-12-07 14:36:30 +0100

    Add files via upload

--- Pendu/PrerequisPendu/main.cpp
+++ Pendu/PrerequisPendu/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <string>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    SetConsoleOutputCP(65001);
+    auto chaine= string("bonjouuur"), chaine2= string("Cà va ?");
+    auto chaine3 = chaine + chaine2;
+    cout << chaine3 << endl;
     return 0;
 }
