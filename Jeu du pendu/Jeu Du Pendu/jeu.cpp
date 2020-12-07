b2cb6e37e607d6311590b47ea2b8c95dfa89c445
N0str4 <aymerickvalois@yahoo.fr>
2020-12-07 15:18:44 +0100

    Add files via upload

---
+++ Jeu du pendu/Jeu Du Pendu/jeu.cpp
@@ -0,0 +1,13 @@
+#include "jeu.h"
+#include <iostream>
+
+
+using namespace std;
+
+void afficherLettresTrouvees(string_view lettres){
+    for(auto lettre: lettres){
+
+        cout<<lettre << "";
+
+    }
+}
